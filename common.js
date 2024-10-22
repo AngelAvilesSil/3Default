8b905224510936230d195d1cc3fd8250971701e1
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2024-10-21 21:25:51 -0400

    Working and nice index page

---
+++ common.js
@@ -0,0 +1,48 @@
+function includeHTML() {
+  const includeElements = document.querySelectorAll('[data-include]');
+  
+  includeElements.forEach(el => {
+    const file = el.getAttribute("data-include");
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        el.innerHTML = data;
+        if (el.id === "header") {
+          loadHeaderContent();
+        }
+      })
+      .catch(err => console.error("Failed to load file", err));
+  });
+}
+
+function loadHeaderContent() {
+  const pageName = document.body.getAttribute('data-page'); // Get current page
+  const menuFiles = {
+    "index": "templates/menus/menu_index.html",
+    "dashboard": "templates/menus/menu_dashboard.html",
+    // Add more cases for different pages as needed
+  };
+
+  const menuFile = menuFiles[pageName] || menuFiles['index']; // Default to index menu if no match
+
+  // Fetch and load the appropriate menu HTML file
+  fetch(menuFile)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('menuItems').innerHTML = data;
+    })
+    .catch(err => console.error("Failed to load menu file", err));
+}
+
+// Call the function on page load
+window.onload = includeHTML;
+
+
+function myFunction() {
+	var x = document.getElementById("demo");
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
